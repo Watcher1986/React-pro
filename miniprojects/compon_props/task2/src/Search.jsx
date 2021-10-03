5d20258f6aa2eff8253cf85cd288168ba2282954
Denis <Deniss2007@gmail.com>
2021-10-03 18:03:19 +0300

    c

--- miniprojects/compon_props/task2/src/Search.jsx
+++ miniprojects/compon_props/task2/src/Search.jsx
@@ -4,7 +4,9 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you ?`}</h1>
+        <h1 className="search__title">
+          Hello, {this.props.name}. What to search for you ?
+        </h1>
         <div className="search__field">
           <input type="text" className="search__input" />
           <button className="search__button">Search</button>
