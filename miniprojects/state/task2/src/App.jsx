366f9491b262790406eefbf37b002c8d8df8f7f3
Denis <Deniss2007@gmail.com>
2021-10-05 17:01:03 +0300

    offset

--- miniprojects/state/task2/src/App.jsx
+++ miniprojects/state/task2/src/App.jsx
@@ -4,8 +4,8 @@ import Clock from './Clock.jsx';
 const App = () => {
   return (
     <>
-      <Clock location={'Kyiv'} offset={0} />
-      <Clock location={'London'} offset={2} />
+      <Clock location={'Kyiv'} offset={2} />
+      <Clock location={'London'} offset={0} />
       <Clock location={'New York'} offset={-5} />
     </>
   );
