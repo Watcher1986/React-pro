555e812b68ae998486212084d874ea12c6a7a675
Denis <Deniss2007@gmail.com>
2021-10-09 23:51:35 +0300

    ternary

---
+++ miniprojects/conditional_rendering/task4/src/Status.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Offline from './Offline';
+import Online from './Online';
+
+class Status extends Component {
+  state = {
+    isOnline: true,
+  };
+
+  render() {
+    return <div className="status">{this.state.isOnline ? <Online /> : <Offline />}</div>;
+  }
+}
+
+export default Status;
