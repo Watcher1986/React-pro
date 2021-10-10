9e6265a5199b21196e44632c08181d9d98f946ad
Denis <Deniss2007@gmail.com>
2021-10-10 17:12:47 +0300

    dev

--- miniprojects/conditional_rendering/task6/src/GuestGreeting.jsx
+++ miniprojects/conditional_rendering/task6/src/GuestGreeting.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const Emoji = props => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={props.label ? props.label : ''}
+    aria-hidden={props.label ? 'false' : 'true'}
+  >
+    {props.symbol}
+  </span>
+);
+
 const GuestGreeting = () => {
-  return <h1>Hi Stranger. Please login</h1>;
+  return (
+    <h1>
+      Hi Stranger. Please login <Emoji symbol="🚀" />
+    </h1>
+  );
 };
 
 export default GuestGreeting;
--- miniprojects/conditional_rendering/task6/src/UserGreeting.jsx
+++ miniprojects/conditional_rendering/task6/src/UserGreeting.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const Emoji = props => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={props.label ? props.label : ''}
+    aria-hidden={props.label ? 'false' : 'true'}
+  >
+    {props.symbol}
+  </span>
+);
+
 const UserGreeting = () => {
-  return <h1>Hello, User ;)</h1>;
+  return (
+    <h1>
+      Hello, User <Emoji symbol="😎" />
+    </h1>
+  );
 };
 
 export default UserGreeting;
