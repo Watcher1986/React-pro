587d904d9bc3113e3ab5e36d8689fdbdf20d0f99
Denis <Deniss2007@gmail.com>
2021-10-10 10:33:38 +0300

    v

--- miniprojects/conditional_rendering/task4/src/Status.jsx
+++ miniprojects/conditional_rendering/task4/src/Status.jsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Offline from './Offline';
 import Online from './Online';
 
-class Status extends Component {
-  state = {
-    isOnline: [false, true],
-  };
-
-  render() {
-    return <div className="status">{this.state.isOnline[1] ? <Online /> : <Offline />}</div>;
-  }
-}
+const Status = ({ isOnline }) => {
+  return <div className="status">{isOnline ? <Online /> : <Offline />}</div>;
+};
 
 export default Status;
