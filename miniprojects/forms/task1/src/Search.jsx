3d53e1f16d0d19c77059ee70fc13071e868605b8
Denis <Deniss2007@gmail.com>
2021-10-13 22:09:02 +0300

    f.i.v.

--- miniprojects/forms/task1/src/Search.jsx
+++ miniprojects/forms/task1/src/Search.jsx
@@ -16,7 +16,7 @@ class Search extends Component {
 
   search = e => {
     e.preventDefault();
-    console.log(this.state.value);
+    alert(`Search text: ${this.state.value}`);
   };
 
   render() {
