e4f56ceb4a62264dea702fec5aee08ce9e3ef346
Denis <Deniss2007@gmail.com>
2021-10-14 19:35:52 +0300

    gff

--- miniprojects/forms/task2/src/UserForm.jsx
+++ miniprojects/forms/task2/src/UserForm.jsx
@@ -29,10 +29,10 @@ class UserForm extends Component {
   render() {
     // const createUser = this.props.createUser;
 
-    const createUser = this.props.onSubmit(this.state);
+    // const createUser = this.props.onSubmit(this.state);
 
     return (
-      <form className="login-form" onSubmit={() => createUser(this.state)}>
+      <form className="login-form" onSubmit={this.props.onSubmit(this.state)}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
