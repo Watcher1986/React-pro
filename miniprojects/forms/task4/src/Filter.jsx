dae389ffeafa8980642a6b0a4aff1a4538e12221
Denis <Deniss2007@gmail.com>
2021-10-18 04:18:52 +0300

    filter

--- miniprojects/forms/task4/src/Filter.jsx
+++ miniprojects/forms/task4/src/Filter.jsx
@@ -4,7 +4,7 @@ class Filter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: '',
+      inputValue: this.props.filterText,
     };
   }
 
@@ -17,23 +17,23 @@ class Filter extends Component {
   // } else {
   //   usersList = this.state.users;
   // }
-  onInputChange(e) {
+  onInputChange = e => {
     this.setState({
       inputValue: e.target.value,
     });
-    let newCount = null;
-    const inpValue = e.target.value;
-    let usersList = {};
-    const users = this.props.filterText;
-    console.log(users);
-    if (inpValue !== '') {
-      usersList = users.filter(user => inpValue.toLowerCase().includes(user.name.toLowerCase()));
-      newCount = usersList.length;
-    } else {
-      usersList = users;
-    }
-    this.props.onChange(usersList, newCount);
-  }
+    // let newCount = null;
+    // const inpValue = e.target.value;
+    // let usersList = {};
+    // const users = this.props.filterText;
+    // console.log(users);
+    // if (inpValue !== '') {
+    //   usersList = users.filter(user => inpValue.toLowerCase().includes(user.name.toLowerCase()));
+    //   newCount = usersList.length;
+    // } else {
+    //   usersList = users;
+    // }
+    this.props.onChange(e.target.value);
+  };
 
   render() {
     return (
@@ -43,7 +43,7 @@ class Filter extends Component {
           type="text"
           className="filter__input"
           value={this.state.inputValue}
-          onChange={this.onInputChange.bind(this)}
+          onChange={this.onInputChange}
         />
       </div>
     );
--- miniprojects/forms/task4/src/UsersList.jsx
+++ miniprojects/forms/task4/src/UsersList.jsx
@@ -6,42 +6,42 @@ class UsersList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: this.props.users,
-      // filterText: '',
-      nameIncludesCount: null,
+      //users: this.props.users,
+      filterText: 'some text',
+      //nameIncludesCount: null,
     };
   }
 
-  onChangeFilterText(newUsersList, newCount) {
+  onChangeFilterText = newFilterText => {
     this.setState({
-      users: newUsersList,
-      // filterText: newFilterText,
-      nameIncludesCount: newCount,
+      // users: newUsersList,
+      filterText: newFilterText,
+      // nameIncludesCount: newCount,
     });
-  }
+  };
 
   render() {
-    // let nameIncludesCount = null;
-    // const inputText = this.state.filterText;
-    // let usersList = {};
-    // if (inputText !== '') {
-    //   usersList = this.state.users.filter(user =>
-    //     inputText.toLowerCase().includes(user.name.toLowerCase()),
-    //   );
-    //   nameIncludesCount = usersList.length;
-    // } else {
-    //   usersList = this.state.users;
-    // }
+    let nameIncludesCount = null;
+    const inputText = this.state.filterText;
+    let usersList = {};
+    if (inputText !== '') {
+      usersList = this.props.users.filter(user =>
+        inputText.toLowerCase().includes(user.name.toLowerCase()),
+      );
+      nameIncludesCount = usersList.length;
+    } else {
+      usersList = this.props.users;
+    }
 
     return (
       <div>
         <Filter
-          onChange={this.onChangeFilterText.bind(this)}
-          filterText={this.props.users}
-          count={this.state.nameIncludesCount || 0}
+          onChange={this.onChangeFilterText}
+          filterText={this.state.filterText}
+          count={nameIncludesCount || 0}
         />
         <ul className="users">
-          {this.state.users.map(user => (
+          {usersList.map(user => (
             <User key={user.id} {...user} />
           ))}
         </ul>
