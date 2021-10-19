3eeb529eafdaaedaa880e9b3c419b22331eea281
Denis <Deniss2007@gmail.com>
2021-10-19 19:29:30 +0300

    lifting state up

--- miniprojects/liftingStateUp/task2/src/App.jsx
+++ miniprojects/liftingStateUp/task2/src/App.jsx
@@ -24,9 +24,9 @@ class Page extends Component {
     const { userData } = this.state;
     return (
       <div className="page">
-        <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+        <h1 className="title">{`Hello, ${userData.firstName}`}</h1>
         <main className="content">
-          <ShoppingCart userData={userData} />
+          <ShoppingCart userName={userData.firstName} />
           <Profile userData={userData} handleChange={this.handleChange} />
         </main>
       </div>
