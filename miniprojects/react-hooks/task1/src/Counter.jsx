1b4578edd5f3936a8de8ebf233b35974fa2cf1e7
Denis <Deniss2007@gmail.com>
2021-10-25 08:17:56 +0300

    hooks

--- miniprojects/react-hooks/task1/src/Counter.jsx
+++ miniprojects/react-hooks/task1/src/Counter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-  const [counter, setCounter] = useState(7);
+  const [counter, setCounter] = useState(0);
   return (
     <div className="counter">
       <button className="counter__button" onClick={() => setCounter(counter - 1)}>
@@ -18,48 +18,3 @@ const Counter = () => {
 };
 
 export default Counter;
-
-// class Counter extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       counter: 0,
-//     };
-
-//     this.increment = this.increment.bind(this);
-//   }
-
-//   decrement() {
-//     this.setState({
-//       counter: this.state.counter - 1,
-//     });
-//   }
-
-//   increment() {
-//     this.setState({
-//       counter: this.state.counter + 1,
-//     });
-//   }
-
-//   reset = () => {
-//     this.setState({
-//       counter: 0,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div className="counter">
-//         <button className="counter__button" onClick={() => this.decrement()}>
-//           -
-//         </button>
-//         <span className="counter__value" onClick={this.reset}>
-//           {this.state.counter}
-//         </span>
-//         <button className="counter__button" onClick={this.increment}>
-//           +
-//         </button>
-//       </div>
-//     );
-//   }
-// }
